91211f6bddb2e9c3184ea8c6adfc793d397acf46
Alireza <74738973+itsnyx@users.noreply.github.com>
2026-05-21 01:20:15 +0330

    fixed ios build

--- ios/Classes/HTKLineView.swift
+++ ios/Classes/HTKLineView.swift
@@ -109,7 +109,12 @@ class HTKLineView: UIScrollView, UIGestureRecognizerDelegate {
 
     // Loading spinner shown at the left edge when fetching older candles.
     private lazy var loadingSpinner: UIActivityIndicatorView = {
-        let spinner = UIActivityIndicatorView(style: .medium)
+        let spinner: UIActivityIndicatorView
+        if #available(iOS 13.0, *) {
+            spinner = UIActivityIndicatorView(style: .medium)
+        } else {
+            spinner = UIActivityIndicatorView(style: .white)
+        }
         spinner.color = .white
         spinner.hidesWhenStopped = true
         spinner.alpha = 0
